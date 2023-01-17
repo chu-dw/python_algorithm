c09823ea4672e544cb0ac30d5e8885e2b34858fe
DongWon <ehddnjs1747@daum.net>
2023-01-17 15:56:43 +0900

    1_17 (baekjoon)

---
+++ baekjoon/no25304.py
@@ -0,0 +1,14 @@
+total = input()
+num = input()
+total_check = 0
+for i in range(int(num)):
+    check_p, check_n = input().split()
+    total_check = total_check + (int(check_p) * int(check_n))
+
+
+if total_check == int(total):
+    print('Yes')
+else:
+    print('No')
+
+#input 받을 때 int변환이 편할 듯 
